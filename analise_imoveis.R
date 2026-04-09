b3bc153d2e754ed1c12cbd93652a146553cfebec
mauroaranzate <mauroaranzate@id.uff.br>
2026-04-09 20:53:21 -0300

    Script de leitura e analise descritivas com graficos

--- analise_imoveis.R
+++ analise_imoveis.R
@@ -3,6 +3,10 @@ dados <- read.csv("imoveis.csv", header = TRUE, sep = ";")
 head(dados)
 summary(dados)
 
+mean(dados$preco)
+median(dados$preco)
+sd(dados$preco)
+
 hist(dados$preco,
      main = "Histograma do Preço dos Imóveis",
      xlab = "Preço",
