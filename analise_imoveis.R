164d3d55d2c5f1522dbe2daafeca2617a1a5f43d
mauroaranzate <mauroaranzate@id.uff.br>
2026-04-09 20:48:07 -0300

    Leitura e resumo inicial dos dados de imoveis

--- analise_imoveis.R
+++ analise_imoveis.R
@@ -2,3 +2,17 @@ dados <- read.csv("imoveis.csv", header = TRUE, sep = ";")
 
 head(dados)
 summary(dados)
+
+hist(dados$preco,
+     main = "Histograma do Preço dos Imóveis",
+     xlab = "Preço",
+     col = "lightblue")
+
+plot(dados$metragem, dados$imposto,
+     main = "Imposto vs Metragem",
+     xlab = "Metragem",
+     ylab = "Imposto",
+     pch = 19,
+     col = "blue")
+
+
